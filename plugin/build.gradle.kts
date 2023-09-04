be606833de65a173fe933bae5e7a5e0e8da66f57
Nick Ng <ngyewch@gmail.com>
2023-09-04 13:24:07 +0800

    chore: updated dependencies

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     `java-gradle-plugin`
     `maven-publish`
-    id("ca.cutterslade.analyze") version "1.9.0"
+    id("ca.cutterslade.analyze") version "1.9.1"
     id("com.asarkar.gradle.build-time-tracker") version "4.3.0"
-    id("com.github.ben-manes.versions") version "0.46.0"
+    id("com.github.ben-manes.versions") version "0.47.0"
     id("io.github.ngyewch.git-describe") version "0.2.0"
-    id("com.gradle.plugin-publish") version "1.1.0"
+    id("com.gradle.plugin-publish") version "1.2.1"
     id("se.ascp.gradle.gradle-versions-filter") version "0.1.16"
 }
 
@@ -19,8 +19,8 @@ java {
 dependencies {
     implementation(gradleApi())
 
-    implementation("commons-io:commons-io:2.11.0")
-    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("commons-io:commons-io:2.13.0")
+    implementation("org.apache.commons:commons-lang3:3.13.0")
 }
 
 repositories {
@@ -28,18 +28,15 @@ repositories {
 }
 
 gradlePlugin {
+    website.set("https://github.com/ngyewch/gradle-capsule-plugin")
+    vcsUrl.set("https://github.com/ngyewch/gradle-capsule-plugin.git")
     plugins {
         create("gradle-capsule-plugin") {
             id = "com.github.ngyewch.capsule"
             displayName = "Gradle Capsule Plugin"
             description = "Gradle plugin for Capsule."
             implementationClass = "com.github.ngyewch.gradle.CapsulePlugin"
+            tags.set(listOf("capsule"))
         }
     }
-}
-
-pluginBundle {
-    website = "https://github.com/ngyewch/gradle-capsule-plugin"
-    vcsUrl = "https://github.com/ngyewch/gradle-capsule-plugin.git"
-    tags = listOf("capsule")
 }
